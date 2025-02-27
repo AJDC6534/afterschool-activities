9c228dac8598053edf8bc1da2a7d18dc2b02acfb
AJDC6534 <sinon6534@gmail.com>
2025-02-27 23:08:15 +0400

    second commit

---
+++ product.js
@@ -0,0 +1,92 @@
+let products = [
+    {
+        id: 1001,
+        title: "Table Tennis",
+        location: "Deira, Dubai",
+        price: 2000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/tabletennis.jpg',
+        spaces: 25,
+        rating: 3
+    },
+    {
+        id: 1002,
+        title: "Arts and Crafts",
+        location: "Dubai Internet City",
+        price: 1500,
+        image: 'C:/Users/Student/Documents/After school Activities/images/artsandcraft.jpg',
+        spaces: 45,
+        rating: 1
+    },
+    {
+        id: 1003,
+        title: "Volleyball",
+        location: "Ras Al Khaimah",
+        price: 3000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/volleyball.jpg',
+        spaces: 40,
+        rating: 2
+    },
+    {
+        id: 1004,
+        title: "Badminton",
+        location: "Abu Dhabi",
+        price: 1000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/badminton.jpg',
+        spaces: 50,
+        rating: 5
+    },
+    {
+        id: 1005,
+        title: "Chess",
+        location: "Sharjah",
+        price: 2000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/chess.jpg',
+        spaces: 10,
+        rating: 4
+    },
+    {
+        id: 1006,
+        title: "Dance",
+        location: "Ajman",
+        price: 2000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/dance.jpg',
+        spaces: 35,
+        rating: 4
+    },
+    {
+        id: 1007,
+        title: "Karate",
+        location: "Al Barsha 1",
+        price: 2000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/karate.jpg',
+        spaces: 20,
+        rating: 4
+    },
+    {
+        id: 1008,
+        title: "Robotics",
+        location: "Oud Metha",
+        price: 2000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/robotics.jpg',
+        spaces: 40,
+        rating: 4
+    },
+    {
+        id: 1009,
+        title: "Cooking",
+        location: "Jumeirah",
+        price: 2000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/cooking.jpg',
+        spaces: 30,
+        rating: 4
+    },
+    {
+        id: 1010,
+        title: "Music",
+        location: "Satwa",
+        price: 2000,
+        image: 'C:/Users/Student/Documents/After school Activities/images/music.jpg',
+        spaces: 50,
+        rating: 4
+    },
+]
